7ca53110f4c3c2a7f5770aaa05b67933c9a36bf9
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-04 17:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let input1=document.getElementById("player-1");
+let input2=document.getElementById("player-2");
+let btn=document.getElementById("submit");
+let grid=document.getElementById("grid");
+let main=document.getElementsByClassName("container")
+btn.addEventListener("click",()=>{
+    main[0].className="display";
+    for(let i=1;i<9;i++){
+    let div=document.createElement("div");
+    div.className="box";
+    grid.appendChild(div)
+    }
+})
