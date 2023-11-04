296e216f18678864913100e88a592fa7f0346146
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-04 17:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,42 @@ let input1=document.getElementById("player-1");
 let input2=document.getElementById("player-2");
 let btn=document.getElementById("submit");
 let grid=document.getElementById("grid");
-let main=document.getElementsByClassName("container")
+let main=document.getElementsByClassName("container");
+let row=document.getElementsByClassName("row");
+let msg=document.getElementsByClassName("message");
+let c=document.getElementsByClassName("cont");
 btn.addEventListener("click",()=>{
-    main[0].className="display";
-    for(let i=1;i<9;i++){
-    let div=document.createElement("div");
-    div.className="box";
-    grid.appendChild(div)
+    if(input1.value===""){
+        alert("please enter name")
     }
-})
+    else if(input2.value===""){
+         alert("please enter name");
+    }
+    else{
+    main[0].className="display";
+   
+    for(let i=0;i<9;i++){
+    let div=document.createElement("td");
+    div.className="box";
+    div.id=i;
+    let num=Math.floor(i/3);
+   row[num].appendChild(div);
+   
+    }
+    }
+    let count=1;
+      msg[0].textContent=`${input1.value},you're up`
+    c[0].addEventListener("click",(e)=>{
+        if(count%2!==0 && e.target.textContent===""){
+        e.target.textContent="X";
+         msg[0].textContent=`${input.value},you're up`
+        count++;
+        }
+        else if( e.target.textContent===""){
+             e.target.textContent="O";
+             count++;
+              msg[0].textContent=`${input1.value},you're up`
+        }
+    })
+    
+});
